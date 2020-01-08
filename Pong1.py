93c2c34214401f0b5ad8f8dcc8cd97e0b7c68d01
Pavan S Sabnis <pvnsbns@gmail.com>
2020-01-07 19:34:52 -0800

    Set up Screen object

--- Pong1.py
+++ Pong1.py
@@ -1,2 +1,12 @@
+# import turtle graphics module
 import turtle
+
+# Create window instance
+wn = turtle.Screen()
+wn.title('Pong by Pavan Sabnis') # window title
+wn.bgcolor("black") # background color
+wn.setup(width=800, height=600) # window size
+wn.tracer(0) # stops window from updating for faster performace
+
+
 
