4d1c104d5cad5662e625be0cf3d7db3249f75f76
Pavan S Sabnis <pvnsbns@gmail.com>
2020-01-09 10:56:32 -0800

    Add paddle objects

--- Pong1.py
+++ Pong1.py
@@ -9,4 +9,30 @@ wn.setup(width=800, height=600) # window size
 wn.tracer(0) # stops window from updating for faster performace
 
 
+# Paddle A
+paddle_a = turtle.Turtle() # paddle a is a turtle object
+paddle_a.speed(0) # sets turtle speed to maximum possible
+paddle_a.shape('square')
+paddle_a.color('white')
+paddle_a.shapesize(stretch_wid=5, stretch_len=1) # Paddle shape: Default is 20x20 px, now its 100x20 px because of stretch
+paddle_a.penup() # 'pull the pen up' AKA no drawing during turtle movement
+paddle_a.goto(-350,0) # Moves turtle to specified position. Line is drawn if pen is down
+
+
+# Paddle B
+paddle_b = turtle.Turtle() 
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color('white')
+paddle_b.shapesize(stretch_wid=5, stretch_len=1) 
+paddle_b.penup() 
+paddle_b.goto(350,0) 
+
+
+# Ball
+
+
+# Main game loop
+while True:
+    wn.update()
 
